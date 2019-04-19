cb9228da5f8bf079be0ec54e93cd5b7ab8d7da70
wangdanting <18875082742@163.com>
2019-04-19 14:17:07 +0800

    start steps components

--- src/pages/order/orderDetail/index.js
+++ src/pages/order/orderDetail/index.js
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import Tag from '@/components/Tag';
 import StatusBar from '@/components/StatusBar';
+import AddressInfo from '@/components/AddressInfo';
+import Contact from './contact';
 import styles from './style';
 
 class OrderDetail extends PureComponent {
@@ -10,10 +12,25 @@ class OrderDetail extends PureComponent {
   };
 
   render() {
+    const quInfo = {
+      title: '李子坝梁山鸡李子坝梁山鸡李子坝梁山鸡',
+      subtitle: '李子坝梁山鸡李子坝梁山鸡李子坝梁山鸡李子坝梁山鸡李子坝梁山鸡'
+    };
+    const songInfo = {
+      title: '李子坝梁山鸡李子坝梁山鸡李子坝梁山鸡',
+      name: '李子坝'
+    };
     return (
       <View style={styles.page}>
-        <StatusBar date='14-29' time='14:20' status='前取件' money='8.5' />
-        <Tag />
+        <View style={styles.whiteBg}>
+          <StatusBar date='14-29' time='14:20' status='前取件' money='8.5' />
+          <Tag />
+        </View>
+        <View style={styles.map} />
+        <View style={styles.whiteBg}>
+          <AddressInfo quInfo={quInfo} songInfo={songInfo} />
+          <Contact />
+        </View>
       </View>
     );
   }
--- src/pages/order/orderDetail/style.js
+++ src/pages/order/orderDetail/style.js
@@ -4,6 +4,15 @@ const styles = EStyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: '$backgroundColorBase'
+  },
+  map: {
+    width: '100%',
+    height: '400rem',
+    backgroundColor: 'green'
+  },
+  whiteBg: {
+    backgroundColor: '#fff',
+    paddingHorizontal: '30rem'
   }
 });
 
