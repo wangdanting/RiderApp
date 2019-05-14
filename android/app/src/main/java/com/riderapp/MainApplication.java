776e2576bd065a798fe47c2ce44ce45619d27162
大神留条命 <xusj10@gmail.com>
2019-05-14 15:50:08 +0800

    Android: 升级组件

--- android/app/src/main/java/com/riderapp/MainApplication.java
+++ android/app/src/main/java/com/riderapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.riderapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNUpdateVersionModulePackage;
 import cn.qiuxiang.react.amap3d.AMap3DPackage;
 import com.apsl.versionnumber.RNVersionNumberPackage;
 import com.reactlibrary.RNUpdateVersionModulePackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNUpdateVersionModulePackage(),
             new AMap3DPackage(),
             new RNVersionNumberPackage(),
             new RNUpdateVersionModulePackage(),
